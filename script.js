51b47b81764a75762eedf2ac02d0d730b05b26ef
LT-boy <aurimas.jasius@gmail.com>
2022-08-24 21:44:56 +0300

    Add clear button logic; add reset function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const numbers = document.querySelectorAll('[data-number]');
 const currentOpScreen = document.getElementById('currentOpScreen');
+const clearButton = document.getElementById('button-clear');
 
+
+clearButton.addEventListener('click', clear);
 
 numbers.forEach((button) =>
   button.addEventListener('click', () => appendNumber(button.textContent))
 )
 
+function clear() {
+	currentOpScreen.textContent = '0';
+}
+
 function appendNumber(number) {
+	if (currentOpScreen.textContent === '0') {
+		resetScreen();
+	}
 	currentOpScreen.textContent += number;
+}
+
+function resetScreen() {
+	currentOpScreen.textContent = '';
 }
